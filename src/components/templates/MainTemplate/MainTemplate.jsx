c9f112ea84eb8dbcfa42566c37cb2491b06b0475
Bartek Z <bartekzarzyk@gmail.com>
2022-10-26 22:06:12 +0200

    change grid properties

--- src/components/templates/MainTemplate/MainTemplate.jsx
+++ src/components/templates/MainTemplate/MainTemplate.jsx
@@ -4,13 +4,13 @@ import { Wrapper } from './MainTemplate.styles';
 import styled from 'styled-components';
 
 const SearchBarWrapper = styled.div`
-    grid-row: 1/1;
+    grid-row: 1/2;
     grid-column: 2/3;
     border-bottom: 1px solid ${({theme}) => theme.colors.darkPurple};
 `
 const News = styled.div`
     grid-row: 1/3;
-    grid-column: 3/3;
+    grid-column: 3/4;
     border-left: 1px solid ${({theme}) => theme.colors.darkPurple};
 
 `
